8c027a692d5ee1c3b97a2cb85d8aa05d1b5d83c9
Deepak <deepakmhn1@gmail.com>
2023-07-10 13:59:29 +0100

    initial commit

---
+++ chat-server/src/test/java/server/chat/server/ChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package server.chat.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
